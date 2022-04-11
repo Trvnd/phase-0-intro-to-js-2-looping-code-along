3e15a18fa672d8bed0eb9b884c541cf8fd7ef4e3
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-04-11 01:16:44 -0400

    commit tree 184a0cf41f77650f9157a500fea5f5a4eae6d62e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let messageArray = [];
+    for (let i = 0; i < 3; i++) {
+        let name = array[i];
+        let message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+        messageArray.push(message);
+    }
+    return messageArray;
+}
+
+const count1 = []
+function countDown(count1) {
+    let i = 11
+    while (i > 0) {
+        console.log(i);
+        i--
+    }
+    return count1
+}
+
+
+const count = []
+function runDown(count) {
+    let i = 4
+    while (i >= 0) {
+        console.log(i);
+        i--
+    }
+    return count
+}
+
